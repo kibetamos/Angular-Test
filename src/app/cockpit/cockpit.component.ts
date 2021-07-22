74b3f6029c2892fb47a4c2df3ad55a7ce54addb9
kibet kilel <amos.rotich@strathmore.edu>
2021-07-22 12:11:10 +0300

    Adding cockpit elements

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, ServerContonent: string}>();
+  @Output('bpcreated') blueprintCreated = new EventEmitter<{serverName: string, ServerContonent: string}>();
+
+newServerName ='';
+newServerContent = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+onAddServer(nameInput:HTMLInputElement) {
+  console.log(nameInput);
+  this.serverCreated.emit({
+    serverName: this.newServerName,
+    ServerContonent: this.newServerContent
+  });
+}
+onAddBluePrint(nameInput:HTMLInputElement) {
+  this.blueprintCreated.emit({
+    serverName: this.newServerName,
+    ServerContonent: this.newServerContent
+  });
+}
 }
